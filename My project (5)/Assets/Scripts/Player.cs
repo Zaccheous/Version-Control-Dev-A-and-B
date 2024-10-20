59a0ca137d17006d5d922cbacf01c142610e3e84
Zaccheous <159400282+Zaccheous@users.noreply.github.com>
2024-10-20 23:29:46 +1100

    Dev A Tweaked PlayerMove Controls
    Dev A added a rotation to player. Nothing too dramatic. It was just Dev A's way of showing what he has learnt

--- My project (5)/Assets/Scripts/Player.cs
+++ My project (5)/Assets/Scripts/Player.cs
@@ -5,8 +5,11 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
-    private Vector3 moveDirection = new Vector3(0f, 0f, 0f);
+    [SerializeField] private float power = 0f;
+    [SerializeField] private float turningRate;
+    private Vector3 direction = new Vector3(0f, 0f, 1f);
+    //assigned rotationAxis to Vector3.up which is the shorthand for writing Vector3(0, 1, 0)
+    private Vector3 rotationAxis = Vector3.up;
 
     private PlayerInput actions;
     private InputAction movementAction;
@@ -30,8 +33,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveDirection.x = movementAction.ReadValue<Vector2>().x;
-        moveDirection.z = movementAction.ReadValue<Vector2>().y;
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.Self);
+        // Get input for movement and rotation
+        float acceleration = movementAction.ReadValue<Vector2>().y;
+        float turn = movementAction.ReadValue<Vector2>().x;
+
+        // Move and rotate the object
+        transform.Translate(direction * power * acceleration * Time.deltaTime, Space.Self);
+        transform.Rotate(rotationAxis * turningRate * turn * Time.deltaTime, Space.Self);
     }
 }
