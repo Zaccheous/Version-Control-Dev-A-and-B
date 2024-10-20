33efe77ce891aa548240aa7df228387e3dbafb85
Zaccheous <159400282+Zaccheous@users.noreply.github.com>
2024-10-20 20:29:47 +1100

    Completed Step 3 as Dev B

--- My project (5)/Assets/Scripts/Player.cs
+++ My project (5)/Assets/Scripts/Player.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 1f;
+    private Vector3 moveDirection = new Vector3(0f, 0f, 0f);
+
+    private PlayerInput actions;
+    private InputAction movementAction;
+
+    private void Awake()
     {
-        
+        actions = new PlayerInput();
+        movementAction = actions.GameWorld.Movement;
+    }
+
+    void OnEnable()
+    {
+        movementAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        movementAction.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection.x = movementAction.ReadValue<Vector2>().x;
+        moveDirection.z = movementAction.ReadValue<Vector2>().y;
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.Self);
     }
 }
